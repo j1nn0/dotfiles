03ed47144837856dfeb3ef66028f97d286737193
j1nn0 <j1nn0.github@gmail.com>
2025-02-21 12:59:26 +0900

    🔥 Fix zsh functions

--- .config/zsh/func.zsh
+++ .config/zsh/func.zsh
@@ -4,25 +4,25 @@ function brew-deps() {
     brew list --formula | xargs -P$(expr $(sysctl -n hw.ncpu) - 1) -I{} sh -c 'brew uses --installed {} | wc -l | xargs printf "%20s is used by %2d formulae.\n" {}'
 }
 
-# fbr - checkout git branch
-fbr() {
+# fzfで実装する便利関数
+# https://qiita.com/kamykn/items/aa9920f07487559c0c7e#fzf%E3%81%A7%E5%AE%9F%E8%A3%85%E3%81%99%E3%82%8B%E4%BE%BF%E5%88%A9%E9%96%A2%E6%95%B0
+## fbr - checkout git branch
+function fbr() {
     local branches branch
     branches=$(git branch -vv) &&
         branch=$(echo "$branches" | fzf +m) &&
         git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
 }
-
-# fbrm - checkout git branch (including remote branches)
-fbrm() {
+## fbrm - checkout git branch (including remote branches)
+function fbrm() {
     local branches branch
     branches=$(git branch --all | grep -v HEAD) &&
         branch=$(echo "$branches" |
             fzf-tmux -d $((2 + $(wc -l <<<"$branches"))) +m) &&
         git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
-
-# fshow - git commit browser
-fshow() {
+## fshow - git commit browser
+function fshow() {
     git log --graph --color=always \
         --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
         fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
@@ -45,38 +45,11 @@ function ytm() {
     fi
 }
 
-mkcd() {
-    mkdir $argv && cd $argv
-}
-
-## show parents directory list
-function pwp {
-    local -i depth=0
-    currentDir=$(pwd)
-    echo -e "$fg_bold[red]*: $currentDir$reset"
-
-    while :; do
-        depth+=1
-        currentDir=$(dirname $currentDir)
-        if [ $(($depth % 2)) -eq 1 ]; then
-            echo -e "$fg[green]$depth: $currentDir$reset"
-        else
-            echo -e "$fg[yellow]$depth: $currentDir$reset"
-        fi
-        [[ $currentDir != "/" ]] || break
-    done
-}
-
-## interactive directory selection using pwp
-function cdp() {
-    local parents=()
-    while IFS= read -r line; do
-        parents+=("$line")
-    done < <(pwp | awk -F': ' '/[0-9]+:/ {print $2}')
-
-    local selected=$(printf "%s\n" "${parents[@]}" | fzf --reverse --prompt="Select directory > ")
-
-    if [[ -n "$selected" ]]; then
-        cd "$selected" || echo "Failed to change directory to $selected"
+# mkdirが成功したらcdを実行
+function mkcd() {
+    if [[ -d $1 ]]; then
+        cd $1
+    else
+        mkdir -p $1 && cd $1
     fi
 }
